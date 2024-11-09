b3c9a104b119e127cd1877ffc366c0955d5b282b
eioforkansi <eioforkansi@gmail.com>
2024-11-09 21:25:09 +0100

    Task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,9 @@ from typing import List
 import logging
 
 
+# Define PII fields as a tuple
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 def filter_datum(
         fields: List, redaction: str, message: str, separator: str
         ) -> str:
@@ -31,3 +34,16 @@ class RedactingFormatter(logging.Formatter):
         """Redact specified fields in the log message."""
         msg = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+def get_logger() -> logging.Logger:
+    """To set up the logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    # Set up StreamHandler with RedactingFormatter
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+
+    return logger
