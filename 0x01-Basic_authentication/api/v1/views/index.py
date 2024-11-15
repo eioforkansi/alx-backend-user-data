cbecbcc079f58f5423a061eb214e6ecffe95866a
eioforkansi <eioforkansi@gmail.com>
2024-11-15 23:26:44 +0100

    Task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,7 +7,8 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """ 
+    GET /api/v1/status
     Return:
       - the status of the API
     """
@@ -16,7 +17,8 @@ def status() -> str:
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """
+    GET /api/v1/stats
     Return:
       - the number of each objects
     """
@@ -28,7 +30,16 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
+    """
+    GET /api/v1/unauthorized
     Raise a 401 error by using abort
     """
     abort(401)
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    Raise a 403 error by using abort
+    """
+    abort(403)
